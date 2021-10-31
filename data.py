394163aa7f1dbc783846cd480eb5b76f0631e826
Jet Kan <jetkan.yk@gmail.com>
2021-10-31 23:04:36 +0800

    Implement dataset for 2 subtasks

--- data.py
+++ data.py
@@ -2,8 +2,9 @@
 Loads dataset, preprocesses data and implements the NcgDataset class
 """
 
-from collections import defaultdict
 import os
+import pprint
+from collections import defaultdict
 
 from torch.utils.data import Dataset
 
@@ -14,11 +15,11 @@ def load_data(data_dir):
     `(names, articles, sents, phrases)`.
 
     - `names` maps the sample id to the sample's folder name, e.g. `data/constituency_parsing/0`
-    - `articles` maps the sample id to a list of sentences (each sentence is tokenized into a list
-    of `string` words), the preprocessed NLP scholarly article plaintext
+    - `articles` maps the sample id to a list of sentences, the NLP scholarly article plaintext
     - `sents` maps the sample id to a list of contributing sentence ids (0-indexed)
-    - `phrases` maps the sample id to a `dict` of `{sents: phrases}` (each phrase is a list of `string`
-    words), the scientific terms and relational cue phrases extracted from the contributing sentences
+    - `phrases` maps the sample id to a `dict` of `{sents: phrases}` (each phrase is a list of
+    `string`), the scientific terms and relational cue phrases extracted from the contributing
+    sentences
     """
     names = []
     articles = []
@@ -36,8 +37,7 @@ def load_data(data_dir):
             # Load article
             if filename.endswith("-Stanza-out.txt"):
                 with open(os.path.join(dirpath, filename)) as f:
-                    art_str_list = f.read().splitlines()
-                    article = parse_article(art_str_list)
+                    article = f.read().splitlines()
                     articles.append(article)
 
             # Load contributing sentence id
@@ -57,15 +57,6 @@ def load_data(data_dir):
     return names, articles, sents, phrases
 
 
-def parse_article(art_str_list: list[str]):
-    """
-    Parses a list of `string` from article plaintext into a list of list of `string` words.
-    """
-    # TODO: perform extra word token preprocessing here if required
-    # return list(map(lambda art_str: art_str.split(" "), art_str_list))
-    return art_str_list
-
-
 def parse_sent(sent_str_list: list[str]):
     """
     Parses a list of `string` contributing sentence ids into a list of `int`.
@@ -82,7 +73,7 @@ def parse_phrase(phrase_str_list: list[str]):
 
     phrases = defaultdict(list)
     for row in phrase_list:
-        sent = row[0]
+        sent = int(row[0])
         phrase = row[3]
         phrases[sent].append(phrase)
     return phrases
@@ -91,36 +82,81 @@ def parse_phrase(phrase_str_list: list[str]):
 class NcgDataset(Dataset):
     """
     A `PyTorch Dataset` class that accepts a subtask number and a data directory.
+
+    For subtask 1:
+        x = a full article plaintext (a list of strings) \\
+        y = a contributing sentence (a string)
+
+    For subtask 2:
+        x = a contributing sentence (a string) \\
+        y = a list of phrases (a list of strings)
     """
+
+    def init_subtask1(self):
+        raise NotImplementedError
+
+    def init_subtask2(self):
+        raise NotImplementedError
 
     def __init__(self, subtask, data_dir):
         self.subtask = subtask
-        self.names, self.articles, self.sents, self.phrases = load_data(data_dir)
+        self.names, self.articles, sents, phrases = load_data(data_dir)
+
+        # helper for y of subtask 1
+        self.y1 = []  # (sample, sent_id)
+        for sample, sent_ids in enumerate(sents):
+            for sent_id in sent_ids:
+                self.y1.append((sample, sent_id))
+
+        # helper for x, y of subtask 2
+        self.x2 = []  # (sample, sent_id)
+        self.y2 = []  # phrase
+        for sample, phrase_dict in enumerate(phrases):
+            for sent_id, phrase in phrase_dict.items():
+                self.x2.append((sample, sent_id))
+                self.y2.append(phrase)
 
     def __len__(self):
         """
         Returns the number of samples in the dataset.
         """
-        return len(self.names)
+        if self.subtask == 1:
+            return len(self.y1)
+        elif self.subtask == 2:
+            return len(self.y2)
+        else:
+            raise KeyError
+
+    def get_text(self, sample, sent=None):
+        raise NotImplementedError
 
     def __getitem__(self, i):
         """
         Returns the i-th sample's `(x, y)` tuple.
         """
         if self.subtask == 1:
-            return self.articles[i], self.sents[i]
+            sample, sent_id = self.y1[i]
+            x = self.articles[sample]
+            y = self.articles[sample][sent_id]
+
         elif self.subtask == 2:
-            return self.sents[i], self.phrases[i]
+            sample, sent_id = self.x2[i]
+            x = self.articles[sample][sent_id]
+            y = self.y2[i]
+
         else:
             raise KeyError
+
+        return x, y
 
 
 if __name__ == "__main__":
     # Example use case
     dataset1 = NcgDataset(subtask=1, data_dir="data-mini")
     for d in dataset1:
-        print(f"Input: {d[0]}\nOutput: {d[1]}")
+        pprint.pp(d)
 
-    dataset2 = NcgDataset(subtask=1, data_dir="data-mini")
+    print("=============")
+    dataset2 = NcgDataset(subtask=2, data_dir="data-mini")
     for d in dataset2:
-        print(f"Input: {d[0]}\nOutput: {d[1]}")
+        pprint.pp(d)
