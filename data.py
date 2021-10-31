d00476f30db0f2e9bcb1b23d0fe5758d6d5120d6
Jet Kan <jetkan.yk@gmail.com>
2021-10-31 19:27:42 +0800

    Implement data parsers

--- data.py
+++ data.py
@@ -4,43 +4,8 @@ Loads dataset, preprocesses data and implements the NcgDataset class
 
 import os
 
-from torch import Tensor
+import torch
 from torch.utils.data import Dataset
-
-
-def load_data(data_dir):
-    """
-    Loads dataset from directory `data_dir` and returns a tuple `(text, label)`. \\
-    Both `text` and `label` are a `list` of `N` items, where `N` is the number of samples in the 
-    dataset.
-
-    For each sample:
-    - a `text` item is a `list` of sentences, the preprocessed NLP scholarly article plaintext
-    - a `label` item is a `list` of 0-indexed sentence ids, the contributing sentence ids
-    """
-    texts = []
-    labels = []
-
-    for dirpath, dirnames, filenames in os.walk(data_dir):
-        if dirnames != ["info-units", "triples"]:
-            continue
-
-        # Currently located in task folder
-        for filename in filenames:
-            # Load text
-            if filename.endswith("-Stanza-out.txt"):
-                with open(os.path.join(dirpath, filename)) as f:
-                    text = f.read().splitlines()
-                    texts.append(text)
-
-            # Load label
-            if filename == "sentences.txt":
-                with open(os.path.join(dirpath, filename)) as f:
-                    label_str = f.readlines()
-                    label = sorted(map(int, label_str))
-                    labels.append(label)
-
-    return texts, labels
 
 
 def load_task_names(data_dir):
@@ -59,31 +24,70 @@ def load_task_names(data_dir):
     return tasks
 
 
-def tokenize(sentence):
+def load_data(data_dir):
     """
-    Tokenizes a sentence string into a list of word string
+    Loads dataset from directory `data_dir` and returns a tuple `(text, label)`. \\
+    Both `text` and `label` are a list of `N` items, where `N` is the number of samples in the 
+    dataset.
+
+    For each sample:
+    - a `text` item is a list of sentences (one sentence is a list of `string` words), the 
+    preprocessed NLP scholarly article plaintext
+    - a `label` item is a list of sentence ids in `torch.int`, the contributing sentence ids
     """
-    raise NotImplementedError
+    texts = []
+    labels = []
+
+    for dirpath, dirnames, filenames in os.walk(data_dir):
+        if dirnames != ["info-units", "triples"]:
+            continue
+
+        # Currently located in task folder
+        for filename in filenames:
+            # Load text
+            if filename.endswith("-Stanza-out.txt"):
+                with open(os.path.join(dirpath, filename)) as f:
+                    text_str_list = f.read().splitlines()
+                    text = parse_text(text_str_list)
+                    texts.append(text)
+
+            # Load label
+            if filename == "sentences.txt":
+                with open(os.path.join(dirpath, filename)) as f:
+                    label_str_list = f.read().splitlines()
+                    label = parse_label(label_str_list)
+                    labels.append(label)
+
+    return texts, labels
 
 
-def parse(word):
+def parse_text(text_str_list: list[str]):
     """
-    Parses a word string into a torch.int type
+    Parses a list of `string` sentences into a list of list of `string` words.
     """
-    raise NotImplementedError
+    # TODO: perform extra word token preprocessing here if required
+    return list(map(lambda text_str: text_str.split(" "), text_str_list))
+
+
+def parse_label(label_str_list: list[str]):
+    """
+    Parses a list of `string` labels into a list of `torch.int`.
+
+    Note: labels are 0-indexed
+    """
+    return torch.tensor(
+        sorted(map(lambda label_str: int(label_str) - 1, label_str_list))
+    )
 
 
 class NcgDataset(Dataset):
     """
     A `PyTorch Dataset` class that accepts a data directory.
 
-    - `self.task_names`: maps all `N` dataset sample ids to their corresponding task names, e.g. `data/constituency_parsing/0`
+    - `self.task_names`: maps all `N` dataset sample ids to their corresponding task names, e.g.
+    `data/constituency_parsing/0`
     - `self.texts`: `N` lists of preprocessed NLP scholarly article plaintext
     - `self.labels`: `N` lists of contributing sentence ids
-
-    For each sample:
-    - a `text` item is a `list` of sentences, the preprocessed NLP scholarly article plaintext
-    - a `label` item is a `list` of 0-indexed sentence ids, the contributing sentence ids
     """
 
     def __init__(self, data_dir):
@@ -99,6 +103,9 @@ class NcgDataset(Dataset):
     def __getitem__(self, i):
         """
         Returns the i-th sample's `(text, label)` tuple.
+
+        - `text` is a list of `string`
+        - `label` is a list of `torch.int`
         """
         text = self.texts[i]
         label = self.labels[i]
@@ -107,8 +114,10 @@ class NcgDataset(Dataset):
 
 
 if __name__ == "__main__":
-    dataset = NcgDataset("data-small")
+    # Example use case
+    dataset = NcgDataset("data-mini")
+
     for text, label in dataset:
-        for t in text:
-            print(t)
-        print(label)
+        for i in label:
+            # print all contributing sentences
+            print(text[i])
