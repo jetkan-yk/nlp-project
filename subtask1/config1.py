626ff1a4208c7bf94c67fcb4d07726ca3b28e633
Jet Kan <jetkan.yk@gmail.com>
2021-11-09 09:41:00 +0800

    Update config

--- subtask1/config1.py
+++ subtask1/config1.py
@@ -4,10 +4,9 @@ Contains hyperparameters for subtask 1.
 
 
 class Config1:
-    PIPELINE = "classification"
     BATCH_SIZE = 32
+    EPOCHS = 2
     LEARNING_RATE = 5e-5
-    MOMENTUM = 0.9
-    EPOCHS = 10
-    OPTIMIZER = "sgd"
+    OPTIMIZER = "adam"
+    PIPELINE = "classification"
     SAMPLING_STRAT = "oversampling"
--- subtask2/config2.py
+++ subtask2/config2.py
@@ -4,10 +4,10 @@ Contains hyperparameters for subtask 2.
 
 
 class Config2:
-    PIPELINE = "default"  # problem formulation determines data processing eg. "classification", "extractive summarization"
-    BATCH_SIZE = 20
-    LEARNING_RATE = 0.3
-    MOMENTUM = 0.8
+    BATCH_SIZE = 32
     EPOCHS = 10
+    LEARNING_RATE = 0.1
+    MOMENTUM = 0.9
     OPTIMIZER = "sgd"
+    PIPELINE = "default"  # problem formulation determines data processing eg. "classification", "extractive summarization"
     SAMPLING_STRAT = "default"
