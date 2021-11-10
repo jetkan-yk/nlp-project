db6fb0036a464648650402fae043a383bc97b5f2
Jet Kan <jetkan.yk@gmail.com>
2021-11-11 05:45:02 +0800

    Remove dict union operator for below py 3.9

--- config.py
+++ config.py
@@ -41,18 +41,21 @@ DEFAULT = dict(
     TRAIN_RATIO=0.8,
 )
 
-SBERT_ADAM_OSMP_1 = DEFAULT | dict(
-    SUBTASK=1,
-    EPOCHS=2,
-    LEARNING_RATE=2e-5,
-    MODEL=Model.SCIBERT,
-    OPTIMIZER=Optimizer.ADAM,
-    PIPELINE=Pipeline.CLASSIFICATION,
-    SAMPLING=Sampling.OVERSAMPLING,
+SBERT_ADAM_OSMP_1 = dict(
+    **DEFAULT,
+    **dict(
+        SUBTASK=1,
+        EPOCHS=2,
+        LEARNING_RATE=2e-5,
+        MODEL=Model.SCIBERT,
+        OPTIMIZER=Optimizer.ADAM,
+        PIPELINE=Pipeline.CLASSIFICATION,
+        SAMPLING=Sampling.OVERSAMPLING,
+    )
 )
 
-NB_ADAMW_OSMP_1 = SBERT_ADAM_OSMP_1 | dict(
-    MODEL=Model.NAIVE_BAYES, OPTIMIZER=Optimizer.ADAMW
+NB_ADAMW_OSMP_1 = dict(
+    **SBERT_ADAM_OSMP_1, **dict(MODEL=Model.NAIVE_BAYES, OPTIMIZER=Optimizer.ADAMW)
 )
 
 NcgConfigs = [DEFAULT, SBERT_ADAM_OSMP_1, NB_ADAMW_OSMP_1]
