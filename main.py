2216b6a7ad77f82ea386623bf10d663308298848
Jet Kan <jetkan.yk@gmail.com>
2021-11-10 15:26:27 +0800

    Implement argparse for summary

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 """
-Usage: python3 main.py {1 | 2} [--train | --test] [-d data_dir] [-m model_name]
+Usage: python3 main.py {1 | 2} [--train | --test] [--summary] [-d data_dir] [-m model_name] [-s summary_name]
 """
 
 import argparse
 
 import torch
-import wandb
-
-wandb.init(project="ncg", entity="jetkan-yk")
-
 from torch.utils.data.dataset import random_split
 
+import wandb
 from config import NcgConfig
 from data import NcgDataset
 from model import NcgModel
 
 
 def main(args):
+    if args.summary:
+        wandb.init(project="ncg", entity="jetkan-yk", name=args.s)
+
     dataset = NcgDataset(args.subtask, args.d)
     train_data, test_data = train_test_split(dataset)
 
@@ -48,8 +48,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("subtask", choices=[1, 2], type=int, help="choose subtask")
+
     parser.add_argument("-d", default="data", type=str, help="specify data directory")
     parser.add_argument("-m", default="model", type=str, help="specify model name")
+    parser.add_argument("-s", default=None, type=str, help="specify summary name")
+
+    parser.add_argument("--summary", action="store_true", help="enable summary mode")
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--train", action="store_true", help="train model only")
