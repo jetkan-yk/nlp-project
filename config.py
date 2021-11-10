597b45d491399004c36ee0938dab8540e9859b01
Jet Kan <jetkan.yk@gmail.com>
2021-11-11 05:15:38 +0800

    Remove default pipeline

--- config.py
+++ config.py
@@ -3,12 +3,13 @@ Hyperparameter configs
 """
 from enum import Enum, auto
 
+from sklearn.naive_bayes import MultinomialNB
+
 from subtask1.scibert import SciBert
 
 
 class Pipeline(Enum):
     CLASSIFICATION = auto()
-    DEFAULT = auto()
 
 
 class Sampling(Enum):
@@ -23,7 +24,7 @@ class Optimizer(Enum):
 
 
 class Model(Enum):
-    NAIVEBAYES = None  # TODO
+    NAIVEBAYES = MultinomialNB
     SCIBERT = SciBert
 
 
@@ -35,7 +36,7 @@ DEFAULT = dict(
     MODEL=None,
     MOMENTUM=0.9,
     OPTIMIZER=Optimizer.SGD,
-    PIPELINE=Pipeline.DEFAULT,
+    PIPELINE=Pipeline.CLASSIFICATION,
     SAMPLING=Sampling.SHUFFLE,
     TRAIN_RATIO=0.8,
 )
@@ -50,31 +51,8 @@ SBERT_ADAM_OSMP_1 = DEFAULT | dict(
     SAMPLING=Sampling.OVERSAMPLING,
 )
 
-NB_ADAMW_OSMP_1 = DEFAULT | dict(
-    SUBTASK=1,
-    EPOCHS=2,
-    LEARNING_RATE=2e-5,
-    MODEL=Model.NAIVEBAYES,
-    OPTIMIZER=Optimizer.ADAMW,
-    PIPELINE=Pipeline.CLASSIFICATION,
-    SAMPLING=Sampling.OVERSAMPLING,
+NB_ADAMW_OSMP_1 = SBERT_ADAM_OSMP_1 | dict(
+    MODEL=Model.NAIVEBAYES, OPTIMIZER=Optimizer.ADAMW
 )
-
-"""
-TODO: Copy and overwrite this sample new config, then add the new config into NcgConfigs below
-
-NEW_CONFIG = DEFAULT | dict(
-    SUBTASK=None,
-    BATCH_SIZE=32,
-    EPOCHS=10,
-    LEARNING_RATE=0.1,
-    MODEL=None,
-    MOMENTUM=0.9,
-    OPTIMIZER=Optimizer.SGD,
-    PIPELINE=Pipeline.DEFAULT,
-    SAMPLING=Sampling.SHUFFLE,
-    TRAIN_RATIO=0.8,
-)
-"""
 
 NcgConfigs = [DEFAULT, SBERT_ADAM_OSMP_1, NB_ADAMW_OSMP_1]
